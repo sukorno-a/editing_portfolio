f1deec77b18bb6edf2ac41c101c7878010b20718
guavacadofilms <118949305+guavacadofilms@users.noreply.github.com>
2022-12-05 01:16:46 +0000

    step progress bar
    added timeline for contact section

--- app.js
+++ app.js
@@ -5,3 +5,53 @@ menu.addEventListener("click", function(){
     menu.classList.toggle("is-active");
     menuLinks.classList.toggle("active");
 })
+
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+
+// add EventListener
+one.addEventListener("click", () => {
+    document.querySelector(".first").style.display = "block";
+    document.querySelector(".second").style.display = "none";
+    document.querySelector(".third").style.display = "none";
+    document.querySelector(".fourth").style.display = "none";
+    one.classList.add("active");
+    two.classList.remove("active");
+    three.classList.remove("active");
+    four.classList.remove("active");
+});
+
+two.addEventListener("click", () => {
+    document.querySelector(".first").style.display = "none";
+    document.querySelector(".second").style.display = "block";
+    document.querySelector(".third").style.display = "none";
+    document.querySelector(".fourth").style.display = "none";
+    one.classList.remove("active");
+    two.classList.add("active");
+    three.classList.remove("active");
+    four.classList.remove("active");
+});
+
+three.addEventListener("click", () => {
+    document.querySelector(".first").style.display = "none";
+    document.querySelector(".second").style.display = "none";
+    document.querySelector(".third").style.display = "block";
+    document.querySelector(".fourth").style.display = "none";
+    one.classList.remove("active");
+    two.classList.remove("active");
+    three.classList.add("active");
+    four.classList.remove("active");
+});
+
+four.addEventListener("click", () => {
+    document.querySelector(".first").style.display = "none";
+    document.querySelector(".second").style.display = "none";
+    document.querySelector(".third").style.display = "none";
+    document.querySelector(".fourth").style.display = "block";
+    one.classList.remove("active");
+    two.classList.remove("active");
+    three.classList.remove("active");
+    four.classList.add("active");
+});
